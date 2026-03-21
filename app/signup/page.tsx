b685edd11162716483aaed44f6a977bc9acd69e1
Docjby <jbisraeldocuyanan20@gmail.com>
2026-03-21 11:27:13 +0800

    added middleware

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,9 @@
-import { LoginForm } from "@/components/auth/login-form"
-import { GalleryVerticalEndIcon } from "lucide-react"
-import Image from "next/image"
+"use client"
 
-export default function LoginPage() {
+import { SignupForm } from "@/components/signup-form"
+import { GalleryVerticalEndIcon } from "lucide-react"
+
+export default function SignupPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
@@ -11,21 +12,21 @@ export default function LoginPage() {
             <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEndIcon className="size-4" />
             </div>
-            Digitech Solutions.
+            Acme Inc.
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <LoginForm />
+            <SignupForm />
           </div>
         </div>
       </div>
       <div className="relative hidden bg-muted lg:block">
-        {/* <Image
-          // src="/placeholder.svg"
+        <img
+          src="/placeholder.svg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        /> */}
+        />
       </div>
     </div>
   )
